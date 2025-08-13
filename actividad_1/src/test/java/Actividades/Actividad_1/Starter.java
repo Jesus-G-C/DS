1e9d308ea3bc9e3bcff2e10e609f6f6a1d996c55
Jesus-G-C <gsusgaspar243>
2025-08-12 18:04:05 -0600

    Inicio de Actividad 1

---
+++ actividad_1/src/test/java/Actividades/Actividad_1/Starter.java
@@ -0,0 +1,14 @@
+package Actividades.Actividad_1;
+
+import Actividades.Actividad_1.ui.CLI;
+
+public class Starter {
+
+    /**
+     * Inicia el programa
+     * @param args Es el arreglo de cadenas
+    */
+    public static void main(String[] args) {
+        CLI.runApp();
+    } 
+    }
---
+++ actividad_1/src/test/java/Actividades/Actividad_1/ui/CLI.java
@@ -0,0 +1,19 @@
+package Actividades.Actividad_1.ui;
+
+public class CLI {
+
+    public static void runApp(){
+        showMenu();
+
+    }
+
+    public static void showMenu(){
+        System.out.println("""
+            \n ********************* Bienvenido *********************
+               ***************** ¿Que lista deseas? *****************
+               *****************   1.- Enlazadas    *****************
+               ***************  2.- Doble Enlazadas  ****************    
+               """);
+    }
+
+}
