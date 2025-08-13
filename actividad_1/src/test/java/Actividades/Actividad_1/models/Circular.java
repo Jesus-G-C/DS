4c034d23064adf843620cfe40c3742bf1177f380
Jesus-G-C <gsusgaspar243>
2025-08-13 16:42:17 -0600

    Estructura de las clases mejorada Se mejora el esqueleto y se enlazan las clases

--- actividad_1/src/test/java/Actividades/Actividad_1/models/Circular.java
+++ actividad_1/src/test/java/Actividades/Actividad_1/models/Circular.java
@@ -2,22 +2,47 @@ package Actividades.Actividad_1.models;
 
 import java.util.Scanner;
 
+import Actividades.Actividad_1.process.Ball;
+import Actividades.Actividad_1.ui.MAIN;
+
 public class Circular {
 
     public static void round(){
         Scanner scanner = new Scanner(System.in);
         showMenu();
+        int option = scanner.nextInt();scanner.nextLine();
+
+        while (option != 0){
+            switch (option){
+                case 1:
+                    Ball.add();
+                    break;
+
+                case 2:
+                    Ball.del();
+                    break;
+
+                case 3:
+                    Ball.show();
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida");
+            }
+            showMenu();
+            option = scanner.nextInt();scanner.nextLine();
+        }
     }
 
     private static void showMenu(){
         System.out.println("""
             \n   ******************************************************   
-               ********************* Bienvenido *********************
-               ***************** ¿Que lista deseas? *****************
-               *****************   1.- Enlazadas    *****************
-               ***************  2.- Doble Enlazadas  ****************    
-               *****************  3.- Circulares  *******************
-               ********************  0.- Exit  **********************
+               ************** Bienvenido a lista Circular *************
+               ***************** ¿Que deseas hacer? *****************
+               *******************  1.- Agregar  ********************
+               *******************  2.- Eliminar  *******************
+               *******************  3.- Buscar  *********************    
+               ********************  0.- Back  **********************
                ******************************************************""");
     }
 
---
+++ actividad_1/src/test/java/Actividades/Actividad_1/process/Ball.java
@@ -0,0 +1,25 @@
+package Actividades.Actividad_1.process;
+
+
+
+public class Ball {
+
+
+
+    public static void add(){
+        
+    }
+
+    public static void del(){
+        
+    }
+
+    public static void show(){
+        
+    }
+
+
+}
+
+
+
